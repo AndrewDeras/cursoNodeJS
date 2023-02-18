bb87d7244cd81afb5861b8c1cc2a116d573c34f1
Andrew Deras <andrewderas7@gmail.com>
2023-02-17 23:23:25 -0300

    keep pushing

--- 5_projeto1/index.js
+++ 5_projeto1/index.js
@@ -3,17 +3,26 @@ const app = express();
 
 // fazendo o express usar o ejs como view engine
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/:nome/:lang', (req, res) => {
   const nome = req.params.nome;
   const lang = req.params.lang;
   let exibirMsg = false;
+
+  const produtos = [
+    { nome: 'PS5', preco: 5000 },
+    { nome: 'XBOX ONE', preco: 4000 },
+    { nome: 'TV 4K', preco: 3000 }
+  ];
+
   res.render('index', {
     nome,
     lang,
     empressa: 'Guia do programador',
     inscritos: 10000,
-    msg: exibirMsg
+    msg: exibirMsg,
+    produtos: produtos
   });
 });
 

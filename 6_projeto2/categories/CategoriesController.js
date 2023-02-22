ab3db6d1c1dd2a18ba0aeb01730476ad7828700f
andrewDeras <andrewderas7@gmail.com>
2023-02-22 13:00:06 -0300

    keep pushing 22/02/23

--- 6_projeto2/categories/CategoriesController.js
+++ 6_projeto2/categories/CategoriesController.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
-
-router.get('/categories', (req, res) => {
-  res.send('Rota de categorias');
-});
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get('/admin/categories/new', (req, res) => {
-  res.send('Rota para criar uma nova categoria');
+  res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+  let title = req.body.title;
+  if (title != undefined) {
+
+    Category.create(
+      {
+        title: title,
+        slug: slugify(title)
+      }
+    ).then(() => {
+      res.redirect('/')
+    })
+
+  } else {
+    res.redirect('/admin/categories/new');
+  }
 });
 
 module.exports = router;

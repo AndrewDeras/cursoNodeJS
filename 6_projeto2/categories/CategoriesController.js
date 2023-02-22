ed30a698ab1034cbc969e462248855cd866e405a
andrewDeras <andrewderas7@gmail.com>
2023-02-22 15:49:36 -0300

    keep pushing

--- 6_projeto2/categories/CategoriesController.js
+++ 6_projeto2/categories/CategoriesController.js
@@ -17,12 +17,64 @@ router.post('/categories/save', (req, res) => {
         slug: slugify(title)
       }
     ).then(() => {
-      res.redirect('/')
+      res.redirect('/admin/categories')
     })
 
   } else {
     res.redirect('/admin/categories/new');
   }
+});
+
+router.get('/admin/categories', (req, res) => {
+  Category
+    .findAll({ raw: true })
+    .then((categories) => {
+      res.render('admin/categories/index',
+        {
+          categories: categories
+        });
+    });
+});
+
+router.post('/categories/delete', (req, res) => {
+  let id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Category.destroy(
+        {
+          where: { id: id }
+        }).then(() => {
+          res.redirect('/admin/categories');
+        })
+    } else {
+      res.redirect('/admin/categories');//não for um número
+    }
+  } else {
+    res.redirect('/admin/categories');// null
+  }
+});
+
+router.get('/admin/categories/edit/:id', (req, res) => {
+  let id = req.params.id;
+  if (isNaN(id)) {
+    res.redirect('/admin/categories');
+  }
+
+  Category
+    .findByPk(id)
+    .then((category) => {
+      if (category != undefined) {
+        res.render('admin/categories/edit',
+          {
+            category: category
+          });
+      } else {
+        res.redirect('/admin/categories');
+      }
+    })
+    .catch((erro) => {
+      res.redirect('/admin/categories');
+    })
 });
 
 module.exports = router;

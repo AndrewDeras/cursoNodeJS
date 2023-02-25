7b5ba4db6dfaa0b7ff6cfe15f642159d73ba506e
andrewDeras <andrewderas7@gmail.com>
2023-02-24 22:51:32 -0300

    keep pushing 24/02/23

--- 6_projeto2/articles/ArticlesController.js
+++ 6_projeto2/articles/ArticlesController.js
@@ -60,6 +60,52 @@ router.post('/articles/delete', (req, res) => {
   }
 });
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+  let id = req.params.id;
+  if (isNaN(id)) {
+    res.redirect('/admin/articles');
+  }
+
+  Article
+    .findByPk(id)
+    .then((article) => {
+      if (article != undefined) {
+        Category.findAll().then((categories) => {
+          res.render('admin/articles/edit',
+            {
+              article: article,
+              categories: categories
+            });
+        })
+      } else {
+        res.redirect('/admin/articles');
+      }
+    })
+    .catch((erro) => {
+      res.redirect('/admin/articles');
+    })
+});
+
+router.post('/articles/update', (req, res) => {
+  let id = req.body.id;
+  let title = req.body.title;
+  let body = req.body.body;
+  let category = req.body.category;
+
+  Article.update(
+    {
+      title: title,
+      slug: slugify(title),
+      body: body,
+      categoryId: category
+    },
+    {
+      where: { id: id }
+    }).then(() => {
+      res.redirect('/admin/articles');
+    })
+});
+
 
 
 module.exports = router;

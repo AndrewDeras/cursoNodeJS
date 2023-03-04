06a000f56c6066d2b1895f8c6f88adddafd49d5b
andrewDeras <andrewderas7@gmail.com>
2023-03-03 22:40:47 -0300

    keep pushing

--- 11_introducao_API-REST/index.js
+++ 11_introducao_API-REST/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 

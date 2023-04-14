af5c67188bee9ab6d000e5e4e803c60b62b6ed84
AndrewDeras <andrewderas7@gmail.com>
2023-04-14 16:53:09 -0300

    CRUD com knexjs

--- 15_knexJS/index.js
+++ 15_knexJS/index.js
@@ -27,3 +27,67 @@ database.insert(dados).into("games")
   
 */
 
+/**
+ * Select
+
+database.select(["id", "price"]).table("games").then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.log(error);
+});
+
+query dentro de query
+
+database.insert({ name: "Need for speed", price: 89.75 }).into("games")
+.then((data) => {
+  database.select().table("games").then((data) => {
+    console.log(data);
+  }).catch((error) => {
+    console.log(error);
+  });
+}).catch((error) => {
+  console.log(error);
+});
+
+*/
+
+/**
+ * where
+
+database.select()
+.whereRaw("name = 'Formula 1 2022' OR price > 100")
+.table("games").then((data) => {
+  console.log(data);
+}).catch((error) => {
+  console.log(error);
+});
+
+*/
+
+/**
+ * raw
+
+ database.raw("select * from games")
+   .then((data) => { console.log(data); })
+   .catch((error) => { console.log(error); });
+*/
+
+
+/*
+  *Detele
+ 
+  database.where({ id: 4 }).delete().table("games")
+    .then((data) => { console.log(data); })
+    .catch((error) => { console.log(error); });
+*/
+
+
+/**Update
+ 
+ database.where({ id: 1 }).update({ price: 95.99 }).table("games")
+   .then((data) => { console.log(data); })
+   .catch((error) => { console.log(error); });
+
+*/
+
+

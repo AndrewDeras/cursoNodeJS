2fd448d376d97c3e2631ef3cfb2923037a4269a8
AndrewDeras <andrewderas7@gmail.com>
2023-04-13 23:00:53 -0300

    Start with knexjs

---
+++ 15_knexJS/database.js
@@ -0,0 +1,13 @@
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'Aa12345678@',
+    database: 'knexjs'
+  }
+});
+
+module.exports = knex;
+
---
+++ 15_knexJS/index.js
@@ -0,0 +1,29 @@
+const database = require("./database");
+
+/*
+Insert
+
+const dados = [
+  {
+    name: "GTA V",
+    price: 79.90
+  },
+  {
+    name: "Forza Horizon 5",
+    price: 199.90
+  },
+  {
+    name: "Hitman 3",
+    price: 99.90
+  },
+];
+
+database.insert(dados).into("games")
+  .then((data) => {
+    console.log(data);
+  }).catch((error) => {
+    console.log(error);
+  });
+  
+*/
+

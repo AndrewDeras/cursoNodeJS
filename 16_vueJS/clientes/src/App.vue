b04c0fdbcf28e0186456b98a29730bfff782f5d4
AndrewDeras <andrewderas7@gmail.com>
2023-04-15 22:13:16 -0300

    keep pushing

--- 16_vueJS/clientes/src/App.vue
+++ 16_vueJS/clientes/src/App.vue
@@ -1,26 +1,10 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Cliente from "./components/Cliente.vue";
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <h1>Andrew Barbosa Deras</h1>
+  <Cliente />
+</template>
+
+<style scoped></style>
---
+++ 16_vueJS/clientes/src/components/Cliente.vue
@@ -0,0 +1,25 @@
+<script setup>
+let user = {
+  name: "Andrew",
+  age: 25,
+};
+
+let message = "";
+</script>
+
+<template>
+  <div>
+    <h1>Componente cliente</h1>
+    <input v-model="message" placeholder="Me edite" />
+    <p>A mensagem é: {{ message }}</p>
+    <hr />
+    <p>{{ user.name }}</p>
+    <p>{{ user.age }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: purple;
+}
+</style>
